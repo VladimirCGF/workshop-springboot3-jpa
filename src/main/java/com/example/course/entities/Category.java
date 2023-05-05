cac2ceace563fda0d0e1a681068f0d74424cf02c
Vladimir <vladimir_project@hotmail.com>
2023-05-05 14:13:10 -0300

    Product

--- src/main/java/com/example/course/entities/Category.java
+++ src/main/java/com/example/course/entities/Category.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tbl_category")
@@ -17,6 +19,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -54,4 +59,9 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
 }
